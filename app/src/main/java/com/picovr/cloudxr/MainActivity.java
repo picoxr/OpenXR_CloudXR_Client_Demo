c364cae0319e38c54a591b146f9c745b866b585a
picoxr <pico_devsupport@bytedance.com>
2023-06-27 18:00:32 +0800

    Proactively obtain access to external storage

--- app/src/main/java/com/picovr/cloudxr/MainActivity.java
+++ app/src/main/java/com/picovr/cloudxr/MainActivity.java
@@ -1,12 +1,46 @@
 package com.picovr.cloudxr;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.support.v4.app.ActivityCompat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends android.app.NativeActivity {
     static {
-        // Note: Must handle loading of dependent shared libraries manually before
-        // the shared library that depends on them is loaded, since there is not
-        // currently a way to specify a shared library dependency for NativeActivity
-        // via the manifest meta-data.
         System.loadLibrary("openxr_loader");
         System.loadLibrary("CloudXRClientPXR");
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPermission(this);
+    }
+
+    private List<String> checkPermission(Context context, String[] checkList) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < checkList.length; i++) {
+            if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(context, checkList[i])) {
+                list.add(checkList[i]);
+            }
+        }
+        return list;
+    }
+
+    private void requestPermission(Activity activity, String requestPermissionList[]) {
+        ActivityCompat.requestPermissions(activity, requestPermissionList, 100);
+    }
+
+    private void getPermission(Activity activity) {
+        String[] checkList = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+        List<String> needRequestList = checkPermission(activity, checkList);
+        if (needRequestList.isEmpty()) {
+            Log.i("TAG", "No need to apply for storage permission!");
+        } else {
+            requestPermission(activity, needRequestList.toArray(new String[needRequestList.size()]));
+        }
     }
 }
